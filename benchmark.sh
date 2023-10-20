2c8db51bdbda16127db873f2c80ba3513dd92305
Federico Simonetta <22996003+00sapo@users.noreply.github.com>
2023-10-20 11:35:51 +0200

    forgot about benchmark.sh

---
+++ benchmark.sh
@@ -0,0 +1,33 @@
+TIME=300
+echo
+echo "CPU BENCHMARKS"
+echo "------------------------------------------"
+echo "------------------------------------------"
+sysbench cpu --cpu-max-prime=200000 --time=$TIME run 
+
+echo
+echo "FILEIO BENCHMARKS"
+echo "------------------------------------------"
+echo "------------------------------------------"
+
+sysbench fileio --file-total-size=150G prepare
+sysbench fileio --file-total-size=150G --file-test-mode=rndrw -rand-seed=1 --time=$TIME --max-requests=0 run
+sysbench fileio --file-total-size=150G cleanup
+
+echo
+echo "MEMORY BENCHMARKS"
+echo "------------------------------------------"
+echo "------------------------------------------"
+sysbench memory --threads=64 --time=$TIME run
+
+echo
+echo "MUTEX BENCHMARKS"
+echo "------------------------------------------"
+echo "------------------------------------------"
+sysbench mutex --threads=256 --time=$TIME run
+
+echo
+echo "THREADS BENCHMARKS"
+echo "------------------------------------------"
+echo "------------------------------------------"
+sysbench threads --thread-locks=4 --time=$TIME run
